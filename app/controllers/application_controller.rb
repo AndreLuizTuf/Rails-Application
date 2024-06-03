2234f7b3ff6e4b14bbed936a419472b53f675116
AndreLuizTuf <andre.luiz.a.f@hotmail.com>
2024-06-03 18:00:19 -0300

    Corrigindo no aplication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "admin", password: "123456", except => [:index, :show]
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD'], except: [:index, :show]
 end
